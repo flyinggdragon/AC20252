3cc562905f5c9f21c86280c145a0ece3a1681c56
gairin <marco14963@gmail.com>
2025-11-30 15:07:35 -0300

    Máquina de Estados

--- Procedural Animation/Assets/Scripts/State Machine/StateManager.cs
+++ Procedural Animation/Assets/Scripts/State Machine/StateManager.cs
@@ -1,6 +1,41 @@
+using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class StateManager
-{
-    
+public abstract class StateManager<EState> : MonoBehaviour where EState : Enum {
+    protected Dictionary<EState, BaseState<EState>> states = new Dictionary<EState, BaseState<EState>>();
+    protected BaseState<EState> currentState;
+    protected bool isTransitioningState = false;
+
+    private void Start() {
+        currentState.EnterState();
+    }
+    private void Update() {
+        EState nextStateKey = currentState.GetNextState();
+
+        if (!isTransitioningState && nextStateKey.Equals(currentState.StateKey)) {
+            currentState.UpdateState();
+        }
+        else if (!isTransitioningState) {
+            TransitionToState(nextStateKey);
+        }
+    }
+
+    private void TransitionToState(EState stateKey) {
+        isTransitioningState = true;
+        currentState.ExitState();
+        currentState = states[stateKey];
+        currentState.EnterState();
+        isTransitioningState = false;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        currentState.OnTriggerEnter(other);
+    }
+    private void OnTriggerStay(Collider other) {
+        currentState.OnTriggerStay(other);
+        }
+    private void OnTriggerExit(Collider other) {
+        currentState.OnTriggerExit(other);
+    }
 }
