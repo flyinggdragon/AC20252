f8ebe58ecdeef9cd1615e6e8c0c89e0a7e63ed25
gairin <marco14963@gmail.com>
2025-12-09 16:04:56 -0300

    Visualisador OK

--- AudioSpectrum/Assets/Scripts/AudioSpectrum.cs
+++ AudioSpectrum/Assets/Scripts/AudioSpectrum.cs
@@ -4,37 +4,51 @@ using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
 public class AudioSpectrum : MonoBehaviour {
-    public AudioSource source;
+    private AudioSource source;
+    public List<GameObject> objs;
 
     private int numSamples = 512;
     private int numBands = 8;
-    public static float[] samples;
-    public static float[] bands;
+    public float[] samples;
+    public float[] bands;
 
     private void Start() {
+        source = GetComponent<AudioSource>();
+
         samples = new float[numSamples];
         bands = new float[numBands];
     }
 
     private void Update() {
-        source.GetSpectrumData(samples, 0, FFTWindow.Hamming);
+        source.GetSpectrumData(samples, 0, FFTWindow.Blackman);
         GetFrequencyBands();
+
+        ResizeObjs();
     }
 
     private void GetFrequencyBands() {
         int count = 0;
-        
-        for (int i = 0; i < numBands; i++) {
-            float avg = 0;
-            int sampleCount = (int)Mathf.Pow (2, i) * 2;
 
-            for (int j = 0; j < count; j++) {
-                avg += samples[sampleCount] * (sampleCount + 1);
+        for (int i = 0; i < numBands; i++) {
+            float average = 0f;
+
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+            if (i == numBands - 1) sampleCount += 2;
+
+            for (int j = 0; j < sampleCount; j++) {
+                average += samples[count] * (count + 1);
                 count++;
             }
-            
-            avg /= count;
-            bands[i] = avg * 10;
+
+            average /= sampleCount;
+            bands[i] = average * 10f;
+        }
+    }
+
+    private void ResizeObjs() {
+        for (int i = 0; i < numBands; i++) {
+            objs[i].transform.localScale = new(bands[i], bands[i], bands[i]);
         }
     }
 }
+
